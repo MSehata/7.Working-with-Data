5e0c456631fbb0089f474ff64c6b0ef47fe45f06
Moloko Sehata <moloko4@live.com>
2023-10-04 10:00:14 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnSaySomething = New Button()
+        btnCalculateAge = New Button()
+        btnClose = New Button()
+        SuspendLayout()
+        ' 
+        ' btnSaySomething
+        ' 
+        btnSaySomething.Location = New Point(334, 241)
+        btnSaySomething.Name = "btnSaySomething"
+        btnSaySomething.Size = New Size(131, 39)
+        btnSaySomething.TabIndex = 0
+        btnSaySomething.Text = "Say Something"
+        btnSaySomething.UseVisualStyleBackColor = True
+        ' 
+        ' btnCalculateAge
+        ' 
+        btnCalculateAge.Location = New Point(331, 128)
+        btnCalculateAge.Name = "btnCalculateAge"
+        btnCalculateAge.Size = New Size(134, 45)
+        btnCalculateAge.TabIndex = 1
+        btnCalculateAge.Text = "Calculate Age"
+        btnCalculateAge.UseVisualStyleBackColor = True
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Location = New Point(350, 341)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(94, 29)
+        btnClose.TabIndex = 2
+        btnClose.Text = "Close"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnClose)
+        Controls.Add(btnCalculateAge)
+        Controls.Add(btnSaySomething)
+        Name = "Form1"
+        Text = "Working with Data"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnSaySomething As Button
+    Friend WithEvents btnCalculateAge As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnSaySomething_Click(sender As Object, e As EventArgs) Handles btnSaySomething.Click
+        Dim yourMessage As String, age As Int16
+        yourMessage = "Happy Birthday! How aold are you now? "
+        MsgBox(yourMessage)
+
+        age = Val(InputBox("Enter your Age: "))
+        MsgBox("You are now " & age & " years old")
+
+    End Sub
+
+    Private Sub btnCalculateAge_Click(sender As Object, e As EventArgs) Handles btnCalculateAge.Click
+        Dim yearOfBirth As Int32
+        yearOfBirth = Val(InputBox("Which year where you born? "))
+        MsgBox("You are now " & 2023 - yearOfBirth & " years old")
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+End Class
